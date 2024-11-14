9a91d91cd02144a17e7e4627f3ad689c146f8d96
prosch88 <prosch88@googlemail.com>
2024-11-14 16:27:10 +0100

    modified:   ufade_gui.py

--- ufade_gui.py
+++ ufade_gui.py
@@ -1510,8 +1510,6 @@ class MyApp(ctk.CTk):
 
         else:
             self.wachange = ctk.IntVar(self, 0)
-            self.label1 = ctk.CTkLabel(self.dynamic_frame, text="UFADE by Christian Peter -  Output: {dir_top}", text_color="#3f3f3f", height=60, padx=40, font=self.stfont).pack(anchor="w")
-            self.label1.pack(anchor="center")
             self.label2 = ctk.CTkLabel(self.dynamic_frame, text="PuMA Backup", height=60, width=585, font=("standard",24), justify="left")
             self.label2.pack(pady=20)
             if "net.whatsapp.WhatsApp" in app_id_list and "net.whatsapp.WhatsAppSMB" not in app_id_list:
@@ -1532,7 +1530,6 @@ class MyApp(ctk.CTk):
             self.waitvar(self.wachange)
             if self.wachange.get() == 0:
                 return()
-            self.label1.pack_forget()
             self.label2.pack_forget()
             if "net.whatsapp.WhatsApp" in app_id_list and "net.whatsapp.WhatsAppSMB" in app_id_list:  
                 self.text.pack_forget()
